8a763bd9199b8458ff8a1fcedbf30e9b6dce20d3
Luca Somaschini <somas99@gmail.com>
2022-09-09 17:22:05 +0200

    Working also on second file

--- receive_data.php
+++ receive_data.php
@@ -7,6 +7,13 @@
     <title>PHP Badwords: 2</title>
 </head>
 <body>
-    
+
+    <?php
+        $receivedWord = $_POST['userWord'];
+        $paragraph = 'Tempum lorem ipsum dolor sit amet, lorem adipiscing elit, sed do eiusmod lorem incididunt ut labore et lorem magna aliqua.';
+    ?>
+
+    <h1>Ciao <?php echo $receivedWord ?>.</h1>
+
 </body>
 </html>
